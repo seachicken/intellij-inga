5881af948570bc00b5789ac350c12d51f7c0868f
seito.tanaka <seito.tanaka.dev@gmail.com>
2024-04-21 08:39:08 +0900

    Add start/stop actions

---
+++ src/main/kotlin/inga/intellijinga/actions/StopAnalysisAction.kt
@@ -0,0 +1,10 @@
+package inga.intellijinga.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class StopAnalysisAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
