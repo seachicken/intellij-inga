f9443bfb01282d78e8fa77317819033b7b1a2ad5
seito.tanaka <seito.tanaka.dev@gmail.com>
2024-08-20 21:00:26 +0900

    Update to inga v0.21.2

--- src/main/kotlin/inga/intellijinga/IngaService.kt
+++ src/main/kotlin/inga/intellijinga/IngaService.kt
@@ -32,7 +32,7 @@ class IngaService(
 ) {
     companion object {
         const val INGA_IMAGE_NAME = "ghcr.io/seachicken/inga"
-        const val INGA_IMAGE_TAG = "0.21.1-java"
+        const val INGA_IMAGE_TAG = "0.21.2-java"
         const val INGA_UI_IMAGE_NAME = "ghcr.io/seachicken/inga-ui"
         const val INGA_UI_IMAGE_TAG = "0.4.9"
     }
