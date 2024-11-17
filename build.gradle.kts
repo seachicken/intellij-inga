d1c5523e7b9e09ee890ce85d02a799eaa56aab6d
seito.tanaka <seito.tanaka.dev@gmail.com>
2024-11-17 18:48:46 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "inga"
-version = "0.7.0-beta.1"
+version = "0.7.0-beta.2"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/inga/intellijinga/IngaStartupActivity.kt
+++ src/main/kotlin/inga/intellijinga/IngaStartupActivity.kt
@@ -1,6 +1,7 @@
 package inga.intellijinga
 
 import com.esotericsoftware.kryo.kryo5.minlog.Log
+import com.intellij.ide.plugins.PluginManager
 import com.intellij.ide.plugins.PluginStateManager
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
@@ -12,16 +13,18 @@ class IngaStartupActivity(
     private val cs: CoroutineScope
 ) : ProjectActivity {
     override suspend fun execute(project: Project) {
-        PluginStateManager.addStateListener {
-            Log.info("INGA plugin state changed: $it")
-            if (it.pluginId.idString == "inga.intellij-inga") {
+        Log.info("INGA startup")
+//        PluginStateManager.addStateListener {
+//            Log.info("INGA plugin state changed: $it")
+//            if (it.pluginId.idString == "inga.intellij-inga") {
+//                it.descriptorPath
                 cs.launch {
                     project.service<IngaService>().pullNewIngaImage()
                 }
                 cs.launch {
                     project.service<IngaService>().pullNewIngaUiImage()
                 }
-            }
-        }
+//            }
+//        }
     }
 }
